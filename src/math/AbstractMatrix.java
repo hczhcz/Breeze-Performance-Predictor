82ad9907486ba21f02ac6cf01e89a8ce925ba013
hcz <h.cz@qq.com>
2014-07-19 18:13:50 +0800

    replace class Matrix by interface AbstractMatrix and class DataMatrix

---
+++ src/math/AbstractMatrix.java
@@ -0,0 +1,11 @@
+package math;
+
+public interface AbstractMatrix<T extends Number> {
+	public T get(int x, int y);
+
+	public void set(int x, int y, T value);
+
+	public int xSize();
+
+	public int ySize();
+}
---
+++ src/math/DataMatrix.java
@@ -0,0 +1,38 @@
+package math;
+
+import java.util.ArrayList;
+
+public class DataMatrix<T extends Number> implements AbstractMatrix<T> {
+	protected int _xSize;
+	protected int _ySize;
+	protected ArrayList<ArrayList<T>> data;
+
+	public DataMatrix(int x, int y) {
+		_xSize = x;
+		_ySize = y;
+		data = new ArrayList<ArrayList<T>>(y);
+		for (int i = 0; i < y; ++i) {
+			data.set(i, new ArrayList<T>(x));
+		}
+	}
+
+	@Override
+	public T get(int x, int y) {
+		return data.get(y).get(x);
+	}
+
+	@Override
+	public void set(int x, int y, T value) {
+		data.get(y).set(x, value);
+	}
+
+	@Override
+	public int xSize() {
+		return _xSize;
+	}
+
+	@Override
+	public int ySize() {
+		return _ySize;
+	}
+}
