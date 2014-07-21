c490f481d4face3f9e7729f6bb73ad9da0eeabd9
hcz <h.cz@qq.com>
2014-07-21 16:28:44 +0800

    apply new classes to PerformanceCalc

--- src/performance_calc/PerformanceCalcContainer.java
+++ src/performance_calc/PerformanceCalcContainer.java
@@ -2,92 +2,44 @@ package performance_calc;
 
 import matrix_math.AbstractMatrix;
 import matrix_math.DataMatrix;
-import matrix_math.MathFunc;
-import matrix_math.MathOper;
 import matrix_math.MatrixScanner;
 
-public class PerformanceCalcContainer {
+public class PerformanceCalcContainer extends MatrixScanner<Float> {
 	protected void calc(AbstractMatrix<Float> source, AbstractMatrix<Float> dest) {
 		assert dest.xSize() == source.xSize();
 		assert dest.ySize() == source.ySize();
 
 		final int x = source.xSize();
 		final int y = source.ySize();
-		final float rx = 1 / x;
-		final float ry = 1 / y;
-
-		final MathOper<Float> add = new MathOper<Float>() {
-			@Override
-			public Float f(Float a, Float b) {
-				return a + b;
-			}
-		};
-
-		final MathOper<Float> sub = new MathOper<Float>() {
-			@Override
-			public Float f(Float a, Float b) {
-				return a - b;
-			}
-		};
-
-		final MathOper<Float> addSqr = new MathOper<Float>() {
-			@Override
-			public Float f(Float a, Float b) {
-				return a + b * b;
-			}
-		};
-
-		final MathFunc<Float> rSqrt = new MathFunc<Float>() {
-			@Override
-			public Float f(Float a) {
-				return (float) (1 / Math.sqrt(a));
-			}
-		};
-
-		final MathFunc<Float> divX = new MathFunc<Float>() {
-			@Override
-			public Float f(Float a) {
-				return a * rx;
-			}
-		};
-
-		final MathFunc<Float> divY = new MathFunc<Float>() {
-			@Override
-			public Float f(Float a) {
-				return a * ry;
-			}
-		};
-
-		final MatrixScanner<Float> scanner = new MatrixScanner<Float>();
 
 		final DataMatrix<Float> xSum = new DataMatrix<Float>(x, 1);
 		final DataMatrix<Float> ySum = new DataMatrix<Float>(1, y);
 
-		scanner.reduceY2X(source, xSum, add);
-		scanner.reduceX2Y(source, ySum, add);
+		reduceY2X(source, xSum, MathFuncOperSet.add);
+		reduceX2Y(source, ySum, MathFuncOperSet.add);
 
 		final DataMatrix<Float> xAve = new DataMatrix<Float>(x, 1);
 		final DataMatrix<Float> yAve = new DataMatrix<Float>(1, y);
 
-		scanner.map(xSum, xAve, divY);
-		scanner.map(ySum, yAve, divX);
+		map(xSum, xAve, MathFuncOperSet.mul.curry(1.0f / y));
+		map(ySum, yAve, MathFuncOperSet.mul.curry(1.0f / x));
 
 		final DataMatrix<Float> xAbove = new DataMatrix<Float>(x, y);
 		final DataMatrix<Float> yAbove = new DataMatrix<Float>(x, y);
 
-		scanner.mapY2X(source, xAve, xAbove, sub);
-		scanner.mapX2Y(source, yAve, yAbove, sub);
+		mapY2X(source, xAve, xAbove, MathFuncOperSet.sub);
+		mapX2Y(source, yAve, yAbove, MathFuncOperSet.sub);
 
 		final DataMatrix<Float> xSqrSum = new DataMatrix<Float>(x, 1);
 		final DataMatrix<Float> ySqrSum = new DataMatrix<Float>(1, y);
 
-		scanner.reduceY2X(source, xSqrSum, addSqr);
-		scanner.reduceX2Y(source, ySqrSum, addSqr);
+		reduceY2X(source, xSqrSum, MathFuncOperSet.addSqr);
+		reduceX2Y(source, ySqrSum, MathFuncOperSet.addSqr);
 
 		final DataMatrix<Float> xSimBase = new DataMatrix<Float>(x, 1);
 		final DataMatrix<Float> ySimBase = new DataMatrix<Float>(1, y);
 
-		scanner.map(xSqrSum, xSimBase, rSqrt);
-		scanner.map(ySqrSum, ySimBase, rSqrt);
+		map(xSqrSum, xSimBase, MathFuncOperSet.rSqrt);
+		map(ySqrSum, ySimBase, MathFuncOperSet.rSqrt);
 	}
 }
