6d7b3f05f919a6573faa86d323b81e03df5df453
hcz <h.cz@qq.com>
2014-07-21 17:36:14 +0800

    rename MathFuncOperSet to MathToolSet and add new tools

--- src/performance_calc/MathToolSet.java
+++ src/performance_calc/MathToolSet.java
@@ -1,16 +1,31 @@
 package performance_calc;
 
-import matrix_math.MathCurryingOper;
+import matrix_math.MathAppliedOper;
+import matrix_math.MathConditionalOper;
+import matrix_math.MathCurryingFunc;
 import matrix_math.MathFunc;
 import matrix_math.MathOper;
 
-public class MathFuncOperSet {
+public class MathToolSet {
 	protected static abstract class OperBase implements MathOper<Float> {
 		@Override
 		public abstract Float f(Float a, Float b);
 
 		public MathFunc<Float> curry(Float value) {
-			return new MathCurryingOper<Float>(this, value);
+			return new MathCurryingFunc<Float>(this, value);
+		}
+
+		public MathOper<Float> apply(MathFunc<Float> func) {
+			return new MathAppliedOper<Float>(this, func);
+		}
+
+		public MathOper<Float> boolCond() {
+			return new MathConditionalOper<Float>(this) {
+				@Override
+				protected final boolean Cond(Float a) {
+					return a > 0;
+				}
+			};
 		}
 	};
 
@@ -40,17 +55,33 @@ public class MathFuncOperSet {
 		}
 	};
 
-	public final static OperBase addSqr = new OperBase() {
+	public final static OperBase div = new OperBase() {
 		@Override
 		public Float f(Float a, Float b) {
-			return a + b * b;
+			assert b != 0; // if x/0, error
+
+			return a / b;
+		}
+	};
+
+	public final static FuncBase bool = new FuncBase() {
+		@Override
+		public Float f(Float a) {
+			return (a > 0) ? 1.0f : 0.0f;
+		}
+	};
+
+	public final static FuncBase sqr = new FuncBase() {
+		@Override
+		public Float f(Float a) {
+			return a * a;
 		}
 	};
 
 	public final static FuncBase rSqrt = new FuncBase() {
 		@Override
 		public Float f(Float a) {
-			return (float) (1 / Math.sqrt(a));
+			return (a > 0) ? (float) (1.0f / Math.sqrt(a)) : 0.0f;
 		}
 	};
 }
