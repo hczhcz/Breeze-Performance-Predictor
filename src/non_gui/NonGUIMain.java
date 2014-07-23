8cb4cb41cd3d467d235e4949c522f3183a96a261
hcz <h.cz@qq.com>
2014-07-23 17:33:35 +0800

    add non-gui main function

--- src/non_gui/NonGUIMain.java
+++ src/non_gui/NonGUIMain.java
@@ -1,12 +1,22 @@
 package non_gui;
 
+import java.io.IOException;
+
+import performance_calc.PerformanceCalcContainer;
+
 public class NonGUIMain {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("test");
-	}
+		final PerformanceCalcContainer calc = new PerformanceCalcContainer();
 
+		try {
+			calc.calcFile(0.5f, 0.5f, 0.5f, "in1.txt", "in2.txt", "out1.txt",
+					"out2.txt");
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
