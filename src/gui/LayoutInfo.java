88b7efc1257e946f5fc398635948d02f9466c42d
hcz <h.cz@qq.com>
2014-07-25 17:41:38 +0800

    work on gui

--- src/gui/LayoutInfo.java
+++ src/gui/LayoutInfo.java
@@ -7,19 +7,10 @@ public final class LayoutInfo {
 	public static int stdWidth = 80;
 	public static int stdHeight = 24;
 
-	public static int windowWidth = 640;
+	public static int windowWidth = 720;
 	public static int windowHeight = 480;
 
 	public static int border = 8;
 	public static int borderHalf = 4;
-
-	/*
-	 * public static int groupWidth = windowWidth - 2 * border; public static
-	 * int fileGroupHeight = 4 * stdHeight + 5 * border; public static int
-	 * lambdaGroupHeight = 3 * stdHeight + 4 * border;
-	 * 
-	 * public static int fileTextWidth = groupWidth - 2 * stdWidth - 4 * border;
-	 * public static int lambdaScaleWidth = groupWidth - 2 * stdWidth - 4 *
-	 * border;
-	 */
+	public static int borderLabel = 4;
 }
--- src/gui/Window.java
+++ src/gui/Window.java
@@ -6,6 +6,7 @@ import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
@@ -18,10 +19,10 @@ public class Window {
 	protected Display display;
 	protected Shell shell;
 	protected Group groupFile;
-	protected Group groupFile1a;
-	protected Group groupFile1b;
-	protected Group groupFile2a;
-	protected Group groupFile2b;
+	protected Composite groupFile1a;
+	protected Composite groupFile1b;
+	protected Composite groupFile2a;
+	protected Composite groupFile2b;
 	protected Label label1a;
 	protected Label label1b;
 	protected Label label2a;
@@ -35,9 +36,9 @@ public class Window {
 	protected Button button2a;
 	protected Button button2b;
 	protected Group groupLambda;
-	protected Group groupLambda3;
-	protected Group groupLambda4;
-	protected Group groupLambda5;
+	protected Composite groupLambda3;
+	protected Composite groupLambda4;
+	protected Composite groupLambda5;
 	protected Label label3l;
 	protected Label label3r;
 	protected Label label4l;
@@ -47,12 +48,13 @@ public class Window {
 	protected Scale scale3;
 	protected Scale scale4;
 	protected Scale scale5;
+	protected Button buttonExec;
 	protected ProgressBar progress;
 
 	public Window() {
 		initWidgets();
 		initLayouts();
-		initSizes();
+		initInnerLayouts();
 		initStyle();
 	}
 
@@ -62,10 +64,10 @@ public class Window {
 		shell = new Shell(display);
 
 		groupFile = new Group(shell, 0);
-		groupFile1a = new Group(groupFile, 0);
-		groupFile1b = new Group(groupFile, 0);
-		groupFile2a = new Group(groupFile, 0);
-		groupFile2b = new Group(groupFile, 0);
+		groupFile1a = new Composite(groupFile, SWT.NONE);
+		groupFile1b = new Composite(groupFile, SWT.NONE);
+		groupFile2a = new Composite(groupFile, SWT.NONE);
+		groupFile2b = new Composite(groupFile, SWT.NONE);
 		label1a = new Label(groupFile1a, 0);
 		label1b = new Label(groupFile1b, 0);
 		label2a = new Label(groupFile2a, 0);
@@ -80,9 +82,9 @@ public class Window {
 		button2b = new Button(groupFile2b, 0);
 
 		groupLambda = new Group(shell, 0);
-		groupLambda3 = new Group(groupLambda, 0);
-		groupLambda4 = new Group(groupLambda, 0);
-		groupLambda5 = new Group(groupLambda, 0);
+		groupLambda3 = new Composite(groupLambda, SWT.NONE);
+		groupLambda4 = new Composite(groupLambda, SWT.NONE);
+		groupLambda5 = new Composite(groupLambda, SWT.NONE);
 		label3l = new Label(groupLambda3, 0);
 		label4l = new Label(groupLambda4, 0);
 		label5l = new Label(groupLambda5, 0);
@@ -92,13 +94,13 @@ public class Window {
 		label3r = new Label(groupLambda3, 0);
 		label4r = new Label(groupLambda4, 0);
 		label5r = new Label(groupLambda5, 0);
+		buttonExec = new Button(groupLambda, 0);
 
 		progress = new ProgressBar(shell, 0);
 	}
 
 	protected void initLayouts() {
 		shell.setMinimumSize(LayoutInfo.windowWidth, LayoutInfo.windowHeight);
-		shell.setSize(LayoutInfo.windowWidth, LayoutInfo.windowHeight);
 
 		shell.setLayout(new FormLayout());
 
@@ -108,15 +110,21 @@ public class Window {
 		groupFilePos.right = new FormAttachment(50, -LayoutInfo.borderHalf);
 		groupFilePos.height = 4 * LayoutInfo.stdHeight + 5 * LayoutInfo.border;
 		groupFile.setLayoutData(groupFilePos);
-		// groupFile.setBounds(0, 0, 100, 100);
 
 		final FormData groupLambdaPos = new FormData();
 		groupLambdaPos.left = new FormAttachment(50, LayoutInfo.borderHalf);
 		groupLambdaPos.top = new FormAttachment(0, LayoutInfo.border);
 		groupLambdaPos.right = new FormAttachment(100, -LayoutInfo.borderHalf);
-		groupLambdaPos.height = 3 * LayoutInfo.stdHeight + 4
+		groupLambdaPos.height = 4 * LayoutInfo.stdHeight + 5
 				* LayoutInfo.border;
 		groupLambda.setLayoutData(groupLambdaPos);
+
+		final FormData progressPos = new FormData();
+		progressPos.left = new FormAttachment(0, LayoutInfo.border);
+		progressPos.right = new FormAttachment(100, -LayoutInfo.border);
+		progressPos.bottom = new FormAttachment(100, -LayoutInfo.border);
+		progressPos.height = LayoutInfo.stdHeight;
+		progress.setLayoutData(progressPos);
 
 		final FillLayout groupLayout = new FillLayout(SWT.VERTICAL);
 		groupLayout.marginWidth = LayoutInfo.border;
@@ -125,21 +133,16 @@ public class Window {
 		groupFile.setLayout(groupLayout);
 		groupLambda.setLayout(groupLayout);
 
-		final FillLayout groupLineLayout = new FillLayout(SWT.HORIZONTAL);
-		groupLineLayout.marginWidth = 0;
-		groupLineLayout.marginHeight = 0;
-		groupLineLayout.spacing = LayoutInfo.border;
-		groupFile1a.setLayout(groupLineLayout);
-		groupFile1b.setLayout(groupLineLayout);
-		groupFile2a.setLayout(groupLineLayout);
-		groupFile2b.setLayout(groupLineLayout);
-		groupLambda3.setLayout(groupLineLayout);
-		groupLambda4.setLayout(groupLineLayout);
-		groupLambda5.setLayout(groupLineLayout);
+		groupFile1a.setLayout(new FormLayout());
+		groupFile1b.setLayout(new FormLayout());
+		groupFile2a.setLayout(new FormLayout());
+		groupFile2b.setLayout(new FormLayout());
+		groupLambda3.setLayout(new FormLayout());
+		groupLambda4.setLayout(new FormLayout());
+		groupLambda5.setLayout(new FormLayout());
 	}
 
-	protected void initSizes() {
-
+	protected void initInnerLayouts() {
 		initOpenFile(label1a, text1a, button1a);
 		initOpenFile(label1b, text1b, button1b);
 		initOpenFile(label2a, text2a, button2a);
@@ -151,20 +154,80 @@ public class Window {
 	}
 
 	protected void initStyle() {
+		label1a.setText("Input 1");
+		label1b.setText("Output 1");
+		label2a.setText("Input 2");
+		label2b.setText("Output 2");
+		button1a.setText("Browse ...");
+		button1b.setText("Browse ...");
+		button2a.setText("Browse ...");
+		button2b.setText("Browse ...");
+		label3l.setText("Mix SIM-2");
+		label4l.setText("Keep SIM-1");
+		label5l.setText("Y-based");
+		label3r.setText("Mix SIM-1");
+		label4r.setText("Keep SIM-2");
+		label5r.setText("X-based");
+		buttonExec.setText("Go");
 	}
 
 	protected void initOpenFile(Label label, Text text, Button button) {
-		label.setSize(LayoutInfo.stdWidth, LayoutInfo.stdHeight);
-		button.setSize(LayoutInfo.stdWidth, LayoutInfo.stdHeight);
+		final FormData lPos = new FormData();
+		lPos.left = new FormAttachment(0);
+		lPos.top = new FormAttachment(0, LayoutInfo.borderLabel);
+		lPos.bottom = new FormAttachment(100, -LayoutInfo.borderLabel);
+		lPos.width = LayoutInfo.stdWidth;
+		label.setLayoutData(lPos);
+
+		final FormData mPos = new FormData();
+		mPos.left = new FormAttachment(0, LayoutInfo.stdWidth
+				+ LayoutInfo.border);
+		mPos.top = new FormAttachment(0);
+		mPos.right = new FormAttachment(100, -LayoutInfo.stdWidth
+				- LayoutInfo.border);
+		mPos.bottom = new FormAttachment(100);
+		text.setLayoutData(mPos);
+
+		final FormData rPos = new FormData();
+		rPos.top = new FormAttachment(0);
+		rPos.right = new FormAttachment(100);
+		rPos.bottom = new FormAttachment(100);
+		rPos.width = LayoutInfo.stdWidth;
+		button.setLayoutData(rPos);
+
+		label.setAlignment(SWT.RIGHT);
 	}
 
 	protected void initSetLambda(Label label1, Scale scale, Label label2) {
-		label1.setSize(LayoutInfo.stdWidth, LayoutInfo.stdHeight);
-		label2.setSize(LayoutInfo.stdWidth, LayoutInfo.stdHeight);
+		final FormData lPos = new FormData();
+		lPos.left = new FormAttachment(0);
+		lPos.top = new FormAttachment(0, LayoutInfo.borderLabel);
+		lPos.bottom = new FormAttachment(100, -LayoutInfo.borderLabel);
+		lPos.width = LayoutInfo.stdWidth;
+		label1.setLayoutData(lPos);
+
+		final FormData mPos = new FormData();
+		mPos.left = new FormAttachment(0, LayoutInfo.stdWidth
+				+ LayoutInfo.border);
+		mPos.top = new FormAttachment(0);
+		mPos.right = new FormAttachment(100, -LayoutInfo.stdWidth
+				- LayoutInfo.border);
+		mPos.bottom = new FormAttachment(100);
+		scale.setLayoutData(mPos);
+
+		final FormData rPos = new FormData();
+		rPos.top = new FormAttachment(0, LayoutInfo.borderLabel);
+		rPos.right = new FormAttachment(100, -LayoutInfo.borderLabel);
+		rPos.bottom = new FormAttachment(100);
+		rPos.width = LayoutInfo.stdWidth;
+		label2.setLayoutData(rPos);
+
+		label1.setAlignment(SWT.RIGHT);
+		label2.setAlignment(SWT.LEFT);
 	}
 
 	public void exec() {
-		shell.pack();
+		shell.setSize(LayoutInfo.windowWidth, LayoutInfo.windowHeight);
 		shell.open();
 
 		while (!shell.isDisposed()) {
