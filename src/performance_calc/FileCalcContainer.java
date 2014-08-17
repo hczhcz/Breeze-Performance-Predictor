8c2b6b07807c3d783c5fb07fe062cb2d7c0d14cb
hcz <h.cz@qq.com>
2014-08-17 16:00:17 +0800

    add onChange handler in FileCalcContainer

--- src/performance_calc/FileCalcContainer.java
+++ src/performance_calc/FileCalcContainer.java
@@ -11,20 +11,35 @@ public class FileCalcContainer extends CalcContainer {
 	public FileMatrix<Float> dest1;
 	public FileMatrix<Float> dest2;
 
+	public void onChanged1() {
+		// could be inherited
+	}
+
+	public void onChanged2() {
+		// could be inherited
+	}
+
 	public void loadFile1(String sourceFile1) throws FileNotFoundException {
 		source1 = new FloatFileMatrix(sourceFile1);
 		dest1 = new FloatFileMatrix(source1.xSize(), source1.ySize());
+
+		onChanged1();
 	}
 
 	public void loadFile2(String sourceFile2) throws FileNotFoundException {
 		source2 = new FloatFileMatrix(sourceFile2);
 		dest2 = new FloatFileMatrix(source2.xSize(), source2.ySize());
+
+		onChanged2();
 	}
 
 	public void calcFile(Float lambda1, Float lambda2, Float lambdaXY,
 			Progress progress) {
 		calc(lambda1, lambda2, lambdaXY, source1, source2, dest1, dest2,
 				progress);
+
+		onChanged1();
+		onChanged2();
 	}
 
 	public void saveFile(String destFile1, String destFile2) throws IOException {
